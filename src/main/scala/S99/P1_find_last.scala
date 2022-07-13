c774d29ad66fb30877820a36598a472ffda49f77
Viacheslav Kondratiuk <vkondratiuk@intellias.com>
2022-07-13 17:28:07 +0300

    99 problems: P1 WILs

--- src/main/scala/S99/P1_find_last.scala
+++ src/main/scala/S99/P1_find_last.scala
@@ -1,22 +1,41 @@
 package S99
 
+import scala.annotation.tailrec
+
 // https://www.thedigitalcatonline.com/blog/2015/04/07/99-scala-problems-01-find-last-element/
 object P1_find_last extends App {
 
+  // WIL: 1. Polymorphism with A as generic
+  // WIL: 2. List(h) to test one element
+  // WIL: 3. split List to head :: tail with unapply
+  @tailrec
   def last[A](l: List[A]): A = l match {
-    case h :: Nil => h
+    case List(h) => h
     case _ :: tail => last(tail)
     case _ => throw new NoSuchElementException
   }
 
-  println(last(List(1,2,3,4)))
-  println(last(List("a", "b", "c")))
+  val empty = List()
+  val one = List(1)
+  val five = List(1, 2, 3, 4, 5)
+  val fiveStrings = List("a", "b", "c", "d", "e")
 
-  List(1,2,3,4) match {
-    case h :: t => println(s"head is $h and tail is $t")
+//  println(last(empty))
+  println(last(one))
+  println(last(five))
+  println(last(fiveStrings))
+
+  class -->(val head: String, val tail: String)
+  object --> {
+    def unapply(data: -->): Option[(String, String)] =
+      Some((data.head, data.tail))
   }
 
-  /*
+  val l = new -->("big", "long")
+  val fake = l match {
+    case h --> t => s"Yes, my head is $h and tail is $t"
+    case _ => "No Match"
+  }
 
-   */
+  println(fake)
 }
