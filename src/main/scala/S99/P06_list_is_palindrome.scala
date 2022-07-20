30cd4f9b8123a4bcb7fcabed4e25b61902398652
Viacheslav Kondratiuk <vkondratiuk@intellias.com>
2022-07-20 18:50:12 +0300

    99 problems: P06 WILs

--- src/main/scala/S99/P06_list_is_palindrome.scala
+++ src/main/scala/S99/P06_list_is_palindrome.scala
@@ -4,17 +4,39 @@ import scala.annotation.tailrec
 
 object P06_list_is_palindrome extends App {
 
-  val l = List(1,2,3,3,2,1)
-  println(s"isPalindrome0: ${l == l.reverse}")
-
-
-  def isPalindrome1[A](l: List[A]): Boolean = {
-    @tailrec
-    def listReverse[A](l: List[A], r: List[A] = Nil): List[A] = l match {
-      case h :: t => listReverse(t, h :: r)
-      case Nil => r
+  // WIL1: :+ List pattern matching to get tail element
+  // WIL2: List(_) test List with one lement
+  def isPalindromeClassic[A](l: List[A]): Boolean = {
+    def reverse[A](l: List[A]): List[A] = {
+      @tailrec
+      def _reverse[A](l: List[A], res: List[A]): List[A] = l match {
+        case Nil => res
+        case h :: t => _reverse(t, h :: res)
+      }
+      _reverse(l, Nil)
     }
-    l == listReverse(l)
+
+    l == reverse(l)
   }
-  println(s"isPalindrome1: ${isPalindrome1(l)}")
+
+  def isPalindrome[A](l: List[A]): Boolean = l match {
+    case Nil => true
+    case List(_) => true
+    case (h :: rem) :+ t => (h == t) && isPalindrome(rem)
+  }
+
+  val l0 = List()
+  val l1 = List(1)
+  val l2 = List(1,2)
+  val l3 = List(1,2,1)
+  val l5 = List(1,2,3,2,1)
+  val l6 = List(1,5,3,2,1)
+
+  assert(isPalindrome(l0))
+  assert(isPalindrome(l1))
+  assert(!isPalindrome(l2))
+  assert(isPalindrome(l3))
+  assert(isPalindrome(l5))
+  assert(!isPalindrome(l6))
+  println(isPalindrome(l6))
 }
