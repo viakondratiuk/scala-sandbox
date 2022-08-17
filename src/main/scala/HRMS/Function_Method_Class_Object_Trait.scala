23abde0ae30c08e8cbd3c070350cf6271feb4989
Viacheslav Kondratiuk <vkondratiuk@intellias.com>
2022-08-17 14:53:25 +0300

    HRMS: Methods

--- src/main/scala/HRMS/Function_Method_Class_Object_Trait.scala
+++ src/main/scala/HRMS/Function_Method_Class_Object_Trait.scala
@@ -8,7 +8,7 @@ object Function_Method_Class_Object_Trait extends App {
   //C. What is the proper place of this concept in a bigger picture?
 
 
-  // Function
+  /*** Function ***/
   // A. Reusable block of logic, where you can specify input and output params.
   // In Scala function is named wrapper over expression
   // B. def <identifier>(<identifier>: <type>[, ... ]): <type> = <expression>
@@ -32,8 +32,26 @@ object Function_Method_Class_Object_Trait extends App {
   }
   println(repeater(5, "Bored..."))
 
-  // Method
-  // Class
+  /*** Method ***/
+  // A. Function which exists inside of an object and
+  // act on data from the object is called a function
+  // It available for any instance of the class
+  // There is no operators in Scala, they all are methods
+  // Operator symbol is a name and bound to type
+  // 2 + 3 => 2.+(3) => Infix Operator Notation
+  // Contains in Class, Case Classes, Traits, Enums, Objects
+
+  // B. <class instance>.<method>[(<parameters>)]
+  // Infix Operator Notation <object> <method> <parameter>
+  val s = "vacation.jpg"
+  val isJPEG = s.endsWith(".jpg")
+  val res = 1.+(2)
+
+  // C. One of the main structure block,
+  // it tightly related to Functions
+
+  /*** Class ***/
+
   // Object
   // Trait
 }
