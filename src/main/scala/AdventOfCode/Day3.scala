08d0b40a53b13aa5837d414d5545b6e53ab9fb8a
Viacheslav Kondratiuk <vkondratiuk@intellias.com>
2022-09-15 12:17:57 +0300

    Day3, Immutable-Side Effect

---
+++ src/main/scala/AdventOfCode/Day3.scala
@@ -0,0 +1,24 @@
+package AdventOfCode
+
+object Day3 extends App {
+  val source = scala.io.Source.fromResource("day3.txt")
+  val (lines1, lines2) = source.getLines().duplicate
+  val size = lines2.size
+
+  val ones = lines1
+    .map(_.split("").map(_.toInt))
+    .foldLeft(Array(0,0,0,0,0,0,0,0,0,0,0,0))(
+      (acc, cur) => acc.zip(cur).map(z => z._1 + z._2)
+    )
+  val zeros = ones.map(size - _)
+
+  val gamma = ones.zip(zeros).map(z => if (z._1 > z._2) 1 else 0)
+  val epsilon = ones.zip(zeros).map(z => if (z._1 < z._2) 1 else 0)
+
+//  println(gamma, gamma.mkString(""), epsilon, epsilon.mkString(""))
+
+  val gDec = Integer.parseInt(gamma.mkString(""), 2)
+  val eDec = Integer.parseInt(epsilon.mkString(""), 2)
+
+  println(gDec * eDec)
+}
---
+++ src/main/scala/HRMS/Immutability_SideEffect.scala
@@ -0,0 +1,36 @@
+package HRMS
+
+object Immutability_SideEffect extends App {
+  /** Immutability SideEffect **/
+
+  /**
+  A. Hierarchy and Related Concepts
+
+  Related to FP, gives us a possibility to design function as \
+   in math
+   Pure function - for same input produces same output, all other side effects
+   change state, read file o db, write, reflect, global vars, calling impure
+   immutable structure guarantee that we not changing a state
+   we are not changing objects but creating new one
+   */
+
+  /**
+  B. What problem solves? Abstract and Real
+
+  More thread-safe, less coupled, predictable
+   */
+
+  /**
+  C. Code Example
+   */
+
+
+  /**
+  D. Bloom Taxonomy
+	- Remember, Understand, Apply, Analyse, Evaluate, Create
+
+	- Apply is interpretation, sketching where the concept fits
+	- Analyze is comparing and contrasting other concepts and facts to new information
+	- Evaluate is discriminating which connections between concepts are the most important.
+   */
+}
