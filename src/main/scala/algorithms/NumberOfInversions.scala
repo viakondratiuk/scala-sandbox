229d9dc9b538f1dcaab5eef22a4a77bdac2ee91e
Viacheslav Kondratiuk <vkondratiuk@intellias.com>
2023-11-07 21:49:12 +0200

    Number of inversions, Quick Sort

--- src/main/scala/algorithms/NumberOfInversions.scala
+++ src/main/scala/algorithms/NumberOfInversions.scala
@@ -1,7 +1,11 @@
 package algorithms
 
+import scala.collection.mutable.ArrayBuffer
+
 object NumberOfInversions extends App {
 
+  // [5, 3, 6, 4, 1, 8, 2, 7]
+  // [_, 3, 6, 4, 1, 8, 2, 7]
   def n_2(l: List[Int]): Int = {
     val inversion = for {
       i <- l.indices
@@ -11,10 +15,58 @@ object NumberOfInversions extends App {
     inversion.sum
   }
 
-  def n_log_n(l: List[Int]): Int = {
-    0
+  // [5, 3, 6, 4, 1, 8, 2, 7]
+  // [3, 4, 5, 6]  [1, 2, 8, 7]
+  // [4, 3, 2, 1, 3, 2, 1]
+  def n_log_n(input: Array[Int]): (Array[Int], Int) = {
+
+    def mergeSort(input: Array[Int]): (Array[Int], Int) =
+      if (input.length <= 1) (input, 0)
+      else {
+        val (left, right) = input.splitAt(input.length / 2)
+
+        val (sortedLeft, invLeft) = mergeSort(left)
+//        println(s"[${sortedLeft.mkString(",")}], $invLeft")
+        val (sortedRight, invRight) = mergeSort(right)
+//        println(s"[${sortedRight.mkString(",")}], $invRight")
+        val (sorted, invSplit) = merge(sortedLeft, sortedRight)
+//        println(s"[${sorted.mkString(",")}], $invLeft, $invRight, $invSplit")
+
+        (sorted, invLeft + invRight + invSplit)
+      }
+
+    def merge(left: Array[Int], right: Array[Int]): (Array[Int], Int) = {
+      var (i, j) = (0, 0)
+      var inv = 0
+      val sorted = ArrayBuffer[Int]()
+
+      while (i < left.length && j < right.length) {
+        if (left(i) > right(j)) {
+          sorted += right(j)
+          j += 1
+          inv += left.length - i
+        } else {
+          sorted += left(i)
+          i += 1
+        }
+      }
+
+      sorted ++= left.drop(i)
+      sorted ++= right.drop(j)
+
+      (sorted.toArray, inv)
+    }
+
+    mergeSort(input)
   }
 
-  val l1 = List(5, 3, 1, 4, 2)
-  println(n_2(l1))
+  val l13 = List(5, 3, 6, 4, 1, 8, 2, 7)
+  val l4 = List(2, 1, 4, 6, 3, 5)
+  println(s"n_2: ${n_2(l4)}")
+
+  val a13 = Array(5, 3, 6, 4, 1, 8, 2, 7)
+  val a1 = Array(2, 1, 4, 6, 3, 5)
+  val (sorted, inv) = n_log_n(a13)
+  println(s"sorted: ${sorted.mkString(", ")}")
+  println(s"n_log_n: $inv")
 }
---
+++ src/main/scala/algorithms/QuickSort.scala
@@ -0,0 +1,42 @@
+package algorithms
+
+object QuickSort extends App {
+
+  def partition(a: Array[Int], start: Int, end: Int): Int = {
+    val pivot = a(end)
+    var i = start - 1
+
+    for (j <- start until end) {
+      if (a(j) < pivot) {
+        i += 1
+        val tmp = a(i)
+        a(i) = a(j)
+        a(j) = tmp
+      }
+    }
+
+    val tmp = a(i + 1)
+    a(i + 1) = a(end)
+    a(end) = tmp
+
+    i + 1
+  }
+
+  def sort(a: Array[Int]): Array[Int] = {
+    def _sort(start: Int, end: Int): Unit = {
+      if (start < end) {
+        val pivotIndex = partition(a, start, end)
+        _sort(start, pivotIndex - 1)
+        _sort(pivotIndex + 1, end)
+      }
+    }
+
+    _sort(0, a.length - 1)
+    a
+  }
+
+  val a9 = Array(5, 3, 6, 4, 1, 8, 2, 7, 9)
+  println(s"quick sort: ${sort(a9).mkString(", ")}")
+  val a8 = Array(5, 3, 6, 4, 1, 8, 2, 7)
+  println(s"quick sort: ${sort(a8).mkString(", ")}")
+}
