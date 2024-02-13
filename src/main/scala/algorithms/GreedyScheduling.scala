e6381c3d26c52a363bf9c0bbfa5e8359e1ea0b59
Viacheslav Kondratiuk <viacheslav.kondratiuk@2xideas.com>
2024-02-13 19:43:23 +0200

    Greedy algo for optimal scheduling problem

---
+++ src/main/scala/algorithms/GreedyScheduling.scala
@@ -0,0 +1,28 @@
+package algorithms
+
+case class Activity(start: Int, end: Int)
+
+object ActivitySelection {
+  def selectActivities(activities: Array[Activity]): Array[Activity] = {
+    val sortedActivities = activities.sortBy(_.end)
+    var lastSelectedEnd = -1
+    val selectedActivities = sortedActivities.filter { activity =>
+      if (activity.start >= lastSelectedEnd) {
+        lastSelectedEnd = activity.end
+        true
+      } else {
+        false
+      }
+    }
+    selectedActivities
+  }
+}
+
+object GreedyScheduling extends App {
+    val activities = Array(Activity(1, 4), Activity(3, 5), Activity(0, 6), Activity(5, 7),
+      Activity(3, 9), Activity(5, 9), Activity(6, 10), Activity(8, 11),
+      Activity(8, 12), Activity(2, 14), Activity(12, 16))
+    val selectedActivities = ActivitySelection.selectActivities(activities)
+    println("Selected activities (start, end):")
+    selectedActivities.foreach(activity => println(s"(${activity.start}, ${activity.end})"))
+}
